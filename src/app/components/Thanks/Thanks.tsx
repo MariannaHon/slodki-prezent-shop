b70d9abbde8d58384adcec6804cb1239e6aca253
Marianna Honcharuk <148065963+MariannaHon@users.noreply.github.com>
2026-05-26 19:35:34 +0200

    fixed rendering thanks and deploy

---
+++ src/app/components/Thanks/Thanks.tsx
@@ -0,0 +1,93 @@
+'use client';
+import css from "./Thanks.module.scss"
+
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
+
+import { useEffect, useState } from 'react';
+import { useAppDispatch } from "@/src/redux/hooks";
+
+import { clearCart } from '@/src/redux/cart/slice';
+
+import Skontaktuj from "../Skontaktuj/Skontaktuj";
+
+import { useSearchParams } from 'next/navigation';
+
+const Thanks = () => {
+
+  const path = usePathname();
+
+  const dispatch = useAppDispatch();
+
+      useEffect(() => {
+        dispatch(clearCart());
+      }, [dispatch]);
+  
+  const searchParams = useSearchParams();
+
+  const sessionId = searchParams.get('session_id');
+
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+
+  const fetchSession = async () => {
+
+    const res = await fetch(
+      `https://slodki-prezent-db.onrender.com/session/${sessionId}`
+    );
+
+    const data = await res.json();
+
+    if (data.paymentStatus === 'paid') {
+      setEmail(data.email);
+
+      dispatch(clearCart());
+    }
+  };
+
+  if (sessionId) {
+    fetchSession();
+  }
+
+}, [sessionId, dispatch]);
+
+  return (
+      <main className={css.thanks}>
+        <nav>
+          <ul className='nav'>
+              <li
+                  className={
+                      path === '/' ? 'nav-active' : 'nav-item'
+                  }
+              >
+                  <Link href="/">Strona główna</Link>
+              </li>
+              <span className={css.arrow}>&gt;</span>
+              <li
+                  className={
+                      path === '/thanks'
+                          ? 'nav-active'
+                          : 'nav-item'
+                  }
+              >
+              Thanks
+              </li>
+          </ul>
+      </nav>
+      <h1 className="main-title">Dziękujemy za zakup!</h1>
+      <p className="sub-title">Twoje zamówienie zostało opłacone.</p>
+      <ul className={css['thanks-list']}>
+        <li className={css['thanks-list-item']}>przygotujemy zamówienie w ciągu 24h</li>
+        <li className={css['thanks-list-item']}>wyślemy email z potwierdzeniem</li>
+        <li className={css['thanks-list-item']}>dostawa 1-3 dni robocze</li>
+      </ul>
+      <p className={css['thanks-email']}>Potwierdzenie zostało wysłane na email: <br />
+        {email}</p>
+      <Skontaktuj />
+    </main>
+    
+  )
+}
+
+export default Thanks
--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -1,93 +1,102 @@
 
 'use client';
 
+import { Suspense } from 'react';
+
+import Thanks from '../components/Thanks/Thanks';
+
 import css from "./page.module.scss"
 
-import Link from "next/link";
-import { usePathname } from 'next/navigation';
+// import Link from "next/link";
+// import { usePathname } from 'next/navigation';
 
-import { useEffect, useState } from 'react';
-import { useAppDispatch } from "@/src/redux/hooks";
+// import { useEffect, useState } from 'react';
+// import { useAppDispatch } from "@/src/redux/hooks";
 
-import { clearCart } from '@/src/redux/cart/slice';
+// import { clearCart } from '@/src/redux/cart/slice';
 
-import Skontaktuj from "../components/Skontaktuj/Skontaktuj";
+// import Skontaktuj from "../components/Skontaktuj/Skontaktuj";
 
-import { useSearchParams } from 'next/navigation';
+// import { useSearchParams } from 'next/navigation';
 
 const ThanksPage = () => {
 
-  const path = usePathname();
+//   const path = usePathname();
 
-  const dispatch = useAppDispatch();
+//   const dispatch = useAppDispatch();
 
-      useEffect(() => {
-        dispatch(clearCart());
-      }, [dispatch]);
+//       useEffect(() => {
+//         dispatch(clearCart());
+//       }, [dispatch]);
   
-  const searchParams = useSearchParams();
+//   const searchParams = useSearchParams();
 
-  const sessionId = searchParams.get('session_id');
+//   const sessionId = searchParams.get('session_id');
 
-  const [email, setEmail] = useState('');
+//   const [email, setEmail] = useState('');
 
-  useEffect(() => {
+//   useEffect(() => {
 
-  const fetchSession = async () => {
+//   const fetchSession = async () => {
 
-    const res = await fetch(
-      `https://slodki-prezent-db.onrender.com/session/${sessionId}`
-    );
+//     const res = await fetch(
+//       `https://slodki-prezent-db.onrender.com/session/${sessionId}`
+//     );
 
-    const data = await res.json();
+//     const data = await res.json();
 
-    if (data.paymentStatus === 'paid') {
-      setEmail(data.email);
+//     if (data.paymentStatus === 'paid') {
+//       setEmail(data.email);
 
-      dispatch(clearCart());
-    }
-  };
+//       dispatch(clearCart());
+//     }
+//   };
 
-  if (sessionId) {
-    fetchSession();
-  }
+//   if (sessionId) {
+//     fetchSession();
+//   }
 
-}, [sessionId, dispatch]);
+// }, [sessionId, dispatch]);
 
   return (
-    <main className={css.thanks}>
-      <nav>
-        <ul className='nav'>
-            <li
-                className={
-                    path === '/' ? 'nav-active' : 'nav-item'
-                }
-            >
-                <Link href="/">Strona główna</Link>
-            </li>
-            <span className={css.arrow}>&gt;</span>
-            <li
-                className={
-                    path === '/thanks'
-                        ? 'nav-active'
-                        : 'nav-item'
-                }
-            >
-            Thanks
-            </li>
-        </ul>
-    </nav>
-    <h1 className="main-title">Dziękujemy za zakup!</h1>
-    <p className="sub-title">Twoje zamówienie zostało opłacone.</p>
-    <ul className={css['thanks-list']}>
-      <li className={css['thanks-list-item']}>przygotujemy zamówienie w ciągu 24h</li>
-      <li className={css['thanks-list-item']}>wyślemy email z potwierdzeniem</li>
-      <li className={css['thanks-list-item']}>dostawa 1-3 dni robocze</li>
-    </ul>
-    <p className={css['thanks-email']}>Potwierdzenie zostało wysłane na email: <br />
-      {email}</p>
-    <Skontaktuj />
-  </main>
+    <Suspense fallback={<p>Loading...</p>}>
+      {/* <main className={css.thanks}>
+        <nav>
+          <ul className='nav'>
+              <li
+                  className={
+                      path === '/' ? 'nav-active' : 'nav-item'
+                  }
+              >
+                  <Link href="/">Strona główna</Link>
+              </li>
+              <span className={css.arrow}>&gt;</span>
+              <li
+                  className={
+                      path === '/thanks'
+                          ? 'nav-active'
+                          : 'nav-item'
+                  }
+              >
+              Thanks
+              </li>
+          </ul>
+      </nav>
+      <h1 className="main-title">Dziękujemy za zakup!</h1>
+      <p className="sub-title">Twoje zamówienie zostało opłacone.</p>
+      <ul className={css['thanks-list']}>
+        <li className={css['thanks-list-item']}>przygotujemy zamówienie w ciągu 24h</li>
+        <li className={css['thanks-list-item']}>wyślemy email z potwierdzeniem</li>
+        <li className={css['thanks-list-item']}>dostawa 1-3 dni robocze</li>
+      </ul>
+      <p className={css['thanks-email']}>Potwierdzenie zostało wysłane na email: <br />
+        {email}</p>
+      <Skontaktuj />
+    </main> */}
+      
+      <Thanks />
+    </Suspense>
+    
   )
 }
 
